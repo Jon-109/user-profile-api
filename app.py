f3cf651ffc8895603ff1e350af8bbb8f84d11912
Jon-109 <jonathanfaranda@gmail.com>
2024-11-15 00:05:03 -0600

    Add endpoint to create a new user

--- app.py
+++ app.py
@@ -22,5 +22,13 @@ def get_user(user_id):
     else:
         return jsonify({"error": "User not found"}), 404
 
+# Endpoint to add a new user
+@app.route('/users', methods=['POST'])
+def create_user():
+    new_user = request.get_json()  # Get JSON data from request
+    new_user["user_id"] = users[-1]["user_id"] + 1 if users else 1  # Generate a new ID
+    users.append(new_user)
+    return jsonify(new_user), 201  # Return the new user with a 201 status
+
 if __name__ == '__main__':
     app.run(debug=True)
